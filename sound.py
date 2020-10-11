06aee00cb7c786b1816258f3eeddab687d4a6813
kev <kev@DESKTOP-2H69HGD.lan1>
2020-10-10 21:59:26 -0700

    added second line

--- sound.py
+++ sound.py
@@ -1,4 +1,5 @@
 import pyttsx3
 speaker = pyttsx3.init()
-speaker.say('this is a test to see if my code can speak this sentence. ')
+speaker.say('this is a test to see if my code can speak this sentence.')
+speaker.say('this is a test to see if my code can speak this sentence.')
 speaker.runAndWait()
